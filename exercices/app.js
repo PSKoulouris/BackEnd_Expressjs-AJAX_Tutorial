2f5855071965e0e1ab18cb53fc5911fc249b1788
PSKoulouris <philippe.koulouris@gmail.com>
2025-10-02 22:19:33 +0200

    AJAX

---
+++ exercices/app.js
@@ -0,0 +1,49 @@
+const express = require("express")
+const path = require("path")
+const fs = require("fs")
+const uuid = require("uuid") //installed with npm install uuid
+
+const app = express()
+
+
+
+app.set('view engine', 'ejs')
+app.set("views", path.join(__dirname,"views"))
+
+
+app.use(express.static("public"))
+app.use(express.urlencoded({extended:false}))
+
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+app.get("/exercices", function(req,res){
+    res.render("exercices")
+})
+
+app.get("/", function(req, res){
+    res.render("home")
+})
+
+app.post("/store-user",function(req,res){
+    const username = req.body
+    username.uId = uuid.v4()
+
+    const fileDataPath = path.join(__dirname, "data", "users.json")
+    const fileData = fs.readFileSync(fileDataPath)
+    const users = JSON.parse(fileData)
+    users.push(username)
+    fs.writeFileSync(fileDataPath, JSON.stringify(users))
+    //res.send("User stored successfully!");
+    res.redirect("/")
+})
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+app.listen(3000)
+
